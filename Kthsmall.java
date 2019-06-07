3a9c00806ec2808d9e3e817c2f100177d5ba982e
saravana <saravanasaru14313@gmail.com>
2019-06-07 10:22:45 +0530

    Update and rename README.md to Kthsmall.java

---
+++ Kthsmall.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+public class Ktnsmall
+{
+	public static void main(String[] args) 
+	{
+	    Scanner input=new Scanner(System.in);
+        int size=input.nextInt();
+        int arr[]=new int[size];
+        int b;
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=input.nextInt();   
+        }
+        int small=input.nextInt();
+        for (int i= 0; i <size; i++) 
+        {
+            for (int j=i+1; j <size; j++)
+            {
+                if (arr[i] > arr[j]) 
+                {
+                    b =  arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = b;
+                }
+            }
+        }
+            System.out.println(arr[small-1]);
+	}
+}
